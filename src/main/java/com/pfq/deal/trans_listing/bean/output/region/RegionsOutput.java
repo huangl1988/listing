22489a0bf0b200d6e273f6eb45134fde1a7d6e7d
huangliang <zhlayql@163.com>
2017-11-16 19:32:33 +0800

    初始化

---
+++ src/main/java/com/pfq/deal/trans_listing/bean/output/region/RegionsOutput.java
@@ -0,0 +1,22 @@
+package com.pfq.deal.trans_listing.bean.output.region;
+
+import com.pfq.deal.trans_listing.bean.output.BaseOutput;
+import lombok.*;
+
+import java.util.List;
+
+/**
+ * Created by steven on 2017/11/16.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper = false)
+public class RegionsOutput extends BaseOutput {
+
+    private List<RegionOutputVo> list;
+
+
+
+}
